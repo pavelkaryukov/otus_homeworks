840a4b23f60fce74a43aa8d2d28cebc800dbbb2b
pavelkaryukov <pavelkaryukov@gmail.com>
2020-02-17 02:48:52 +0300

    вместо std::Array поинтер

--- Otus_3/my_allocator.h
+++ Otus_3/my_allocator.h
@@ -26,11 +26,10 @@ struct MapAllocator
     };
     ~MapAllocator()
     {
-//          if (m_Data != nullptr) {
-//              std::free(m_Data);
-//              m_Data = nullptr;
-//              
-//          }
+          if (m_Data != nullptr) {
+              std::free(m_Data);
+              m_Data = nullptr;
+          }
     };
 
     template<typename U>
@@ -48,9 +47,9 @@ struct MapAllocator
     T *allocate(std::size_t n)
     {
         //std::cout << "allocate: [n = " << n << "]" << std::endl;
-//         if (m_Data == nullptr) {
-//             InitMemory();
-//         }
+         if (m_Data == nullptr) {
+             InitMemory();
+         }
         const std::size_t sizeObj = sizeof(T);
         std::size_t currentPos = m_Pos;
         m_Pos += n;
@@ -90,9 +89,9 @@ struct MapAllocator
     }
 
     //pointer m_Data = nullptr;
-    std::array<value_type, kMaxSize + 1> m_Data;
+    
 private:
-
+    pointer m_Data = nullptr;
     const std::size_t m_MaxSize = kMaxSize + 1;
     //pointer m_CurrentPtr = nullptr;
     std::size_t m_Pos = 0;
@@ -105,139 +104,3 @@ private:
     }
 
 };
-
-//#pragma once
-//#include <iostream>
-//template<typename T, std::size_t MaxSize>
-//struct logging_allocator
-//{
-//    using value_type = T;
-//
-//    using pointer = T * ;
-//    using const_pointer = const T*;
-//    using reference = T & ;
-//    using const_reference = const T&;
-//
-//    template<typename U>
-//    struct rebind
-//    {
-//        using other = logging_allocator<U, MaxSize>;
-//    };
-//
-//    logging_allocator()
-//    {
-//        int stop1 = 0;
-//    };
-//    ~logging_allocator() = default;
-//
-//    template<typename U, std::size_t MaxSize2>
-//    logging_allocator(const logging_allocator<U, MaxSize2>&)
-//    {
-//        int stop1 = 0;
-//    }
-//
-//    T *allocate(std::size_t n)
-//    {
-//        std::cout << "allocate: [n = " << n << "]" << std::endl;
-////         auto p = std::malloc(n * sizeof(T));
-////         if (!p)
-////             throw std::bad_alloc();
-//        const std::size_t currentPos = m_Pos;
-//        m_Pos += n;
-//        return &m_Data[currentPos];
-//    }
-//
-//    void deallocate(T *p, std::size_t n)
-//    {
-//        std::cout << "deallocate: [n  = " << n << "] " << std::endl;
-//        --m_Pos;
-//        //std::free(p);
-//    }
-//
-//    template<typename U, typename ...Args>
-//    void construct(U *p, Args &&...args)
-//    {
-//        std::cout << "construct" << std::endl;
-//        new(p) U(std::forward<Args>(args)...);
-//    };
-//
-//    void destroy(T *p)
-//    {
-//        std::cout << "destroy" << std::endl;
-//        p->~T();
-//    }
-//
-//    std::size_t max_size() const
-//    {
-//        return MaxSize;
-//    }
-//
-//private:
-//
-//};
-
-
-///
-//#pragma once
-//template<typename T, std::size_t MaxSize>
-//struct logging_allocator
-//{
-//    using value_type = T;
-//
-//    using pointer = T * ;
-//    using const_pointer = const T*;
-//    using reference = T & ;
-//    using const_reference = const T&;
-//
-//    template<typename U>
-//    struct rebind
-//    {
-//        using other = logging_allocator<U, MaxSize>;
-//    };
-//
-//    logging_allocator()
-//    {
-//        int stop1 = 0;
-//    };
-//    ~logging_allocator() = default;
-//
-//    template<typename U, std::size_t MaxSize2>
-//    logging_allocator(const logging_allocator<U, MaxSize2>&)
-//    {
-//        int stop1 = 0;
-//    }
-//
-//    T *allocate(std::size_t n)
-//    {
-//        std::cout << "allocate: [n = " << n << "]" << std::endl;
-//        auto p = std::malloc(n * sizeof(T));
-//        if (!p)
-//            throw std::bad_alloc();
-//        return reinterpret_cast<T *>(p);
-//    }
-//
-//    void deallocate(T *p, std::size_t n)
-//    {
-//        std::cout << "deallocate: [n  = " << n << "] " << std::endl;
-//
-//        std::free(p);
-//    }
-//
-//    template<typename U, typename ...Args>
-//    void construct(U *p, Args &&...args)
-//    {
-//        std::cout << "construct" << std::endl;
-//        new(p) U(std::forward<Args>(args)...);
-//    };
-//
-//    void destroy(T *p)
-//    {
-//        std::cout << "destroy" << std::endl;
-//        p->~T();
-//    }
-//
-//    std::size_t max_size() const
-//    {
-//        return MaxSize;
-//    }
-//};
--- Otus_3/otus_3.cpp
+++ Otus_3/otus_3.cpp
@@ -6,34 +6,24 @@
 void TestAllocator()
 {
     auto m = std::map<short, long long, std::less<int>, MapAllocator<std::pair<const short, long long>>>();
-    std::cout << "Map Test" << std::endl;
-    for (int i = 0; i < 10; ++i) {
+    int i = 0;
+    for (; i < 10; ++i) {
         m[i] = 11 * i;
-        std::cout << m[i] << std::endl;
         //std::cout << std::endl;
-    }
-
-    auto v = std::vector<short, MapAllocator<short>>();    
-    std::cout << "Vector Test" << std::endl;
-    for (int i = 0; i < 10; ++i) {
-        v.emplace_back(11*i);
-        std::cout << v[i]<< std::endl;
     }
 }
 //-----------------------------------------------------------------------------
 int main(int, char *[])
 {
     std::cout << "Start Program" << std::endl;
-    TestAllocator();
-// 
-//     long long count=0;
-//     for (int i = 0; i < 5000000; ++i) {
-//         TestAllocator();
-//         ++count;
-//         if (count % 10000) {
-//             std::cout << count << std::endl;
-//         }
-//     }
+    long long count=0;
+    for (int i = 0; i < 5000000; ++i) {
+        TestAllocator();
+        ++count;
+        if (count % 10000==0) {
+            std::cout << count << std::endl;
+        }
+    }
     std::cout << "Close Program" << std::endl;
     return 0;
 }
