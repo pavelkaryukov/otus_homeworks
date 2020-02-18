37951829e960c6313e1f5ec858b199b8d116b2a0
pavelkaryukov <pavelkaryukov@gmail.com>
2020-02-18 22:56:39 +0300

    memory leak test gcc

--- Otus_3/otus_3.cpp
+++ Otus_3/otus_3.cpp
@@ -9,30 +9,31 @@ void TestAllocator()
     int i = 0;
     for (; i < 40; ++i) {
         m[i] = 11 * i;
-        std::cout << m[i] << std::endl;
+        //std::cout << m[i] << std::endl;
     }
 }
 //-----------------------------------------------------------------------------
-int main(int, char *[])
-{
-    std::cout << "Start Program" << std::endl;
-    TestAllocator();
-    std::cout << "Close Program" << std::endl;
-    return 0;
-}
+ int main(int, char *[])
+ {
+     std::cout << "Start Program" << std::endl;
+     long long count=0;
+     for (int i = 0; i < 5000000; ++i) {
+         TestAllocator();
+         ++count;
+         if ((count % 10000)==0) {
+             std::cout << count << std::endl;
+         }
+     }
+     std::cout << "Close Program" << std::endl;
+     return 0;
+ }
 
 //-----------------------------------------------------------------------------
-// int main(int, char *[])
-// {
-//     std::cout << "Start Program" << std::endl;
-//     long long count=0;
-//     for (int i = 0; i < 5000000; ++i) {
-//         TestAllocator();
-//         ++count;
-//         if ((count % 10000)==0) {
-//             std::cout << count << std::endl;
-//         }
-//     }
-//     std::cout << "Close Program" << std::endl;
-//     return 0;
-// }
+//int main(int, char *[])
+//{
+//    std::cout << "Start Program" << std::endl;
+//    TestAllocator();
+//    std::cout << "Close Program" << std::endl;
+//    return 0;
+//}
+
