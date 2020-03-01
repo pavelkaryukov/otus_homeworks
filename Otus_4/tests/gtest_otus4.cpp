08535e482c0f93d27d8cf3188d644dd663ff25ba
pavelkaryukov <pavelkaryukov@gmail.com>
2020-03-02 00:00:00 +0300

    ремонт ветки

--- Otus_4/tests/gtest_otus4.cpp
+++ Otus_4/tests/gtest_otus4.cpp
@@ -1,16 +1,17 @@
 #include <gtest/gtest.h> // googletest header file
 #include "ip_to_str.h"
-
+//-----------------------------------------------------------------------------
 TEST(gtest_print_ip,  PrintTuple)
 {
     std::tuple<int, int, int, int>   tuple1 = { 1, 12, 25, 67 };
     std::tuple<int, short, long long> tuple2 = { 1, 12, 25};
-    auto res11 = MyIP::ToStr<std::tuple<int, int, int, int   >>(tuple1, MyIP::ByteOrder::BigEndian);
-    auto res12 = MyIP::ToStr<std::tuple<int, int, int, int   >>(tuple1, MyIP::ByteOrder::LittleEndian);
-    auto res21 = MyIP::ToStr<std::tuple<int, short, long long>>(tuple2);
-    auto res22 = MyIP::ToStr<std::tuple<int, short, long long>>(tuple2);
 
-   ASSERT_EQ(res11.second, MyIP::ErrorCode::Success);
+    auto res11 = MyIP::ToStr<int, int, int, int >(tuple1, MyIP::ByteOrder::BigEndian);
+    auto res12 = MyIP::ToStr<int, int, int, int>(tuple1, MyIP::ByteOrder::LittleEndian);
+    auto res21 = MyIP::ToStr<int, short, std::uint64_t >(tuple2);
+    auto res22 = MyIP::ToStr<int, short, std::uint64_t >(tuple2);
+
+    ASSERT_EQ(res11.second, MyIP::ErrorCode::Success);
     ASSERT_EQ(res12.second, MyIP::ErrorCode::Success);
 
     ASSERT_NE(res21.second, MyIP::ErrorCode::Success); 
@@ -22,8 +23,7 @@ TEST(gtest_print_ip,  PrintTuple)
     ASSERT_TRUE(res11.first.empty());
     ASSERT_TRUE(res12.first.empty());
 }
-
-
+//-----------------------------------------------------------------------------
 // TEST(gtest_test_otus3, TestAllocator)
 // {
 //     otus::StandardMap mapStandartAllocator;
