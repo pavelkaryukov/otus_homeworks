2cb9ec7c301e76f74d3123801b5cea8adcf248f2
pavelkaryukov <pavelkaryukov@gmail.com>
2020-03-01 23:32:15 +0300

    ремонт сборки

--- Otus_4/otus_4.cpp
+++ Otus_4/otus_4.cpp
@@ -8,7 +8,7 @@ void PrintForOtus()
     auto test1 = char(-1);
     auto test2 = short(0);
     auto test3 = int(2130706433);
-    auto test4 = long long(8875824491850138409);
+    auto test4 = std::uint64_t(8875824491850138409);
     MyIP::PrintIpAddr(MyIP::ToStr<decltype(test1)>(test1));
     MyIP::PrintIpAddr(MyIP::ToStr<decltype(test2)>(test2));
     MyIP::PrintIpAddr(MyIP::ToStr<decltype(test3)>(test3));
