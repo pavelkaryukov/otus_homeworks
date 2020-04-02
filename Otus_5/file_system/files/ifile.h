0ebeb740483178f9845df8a8fb263f1cf9948ce7
pavelkaryukov <pavelkaryukov@gmail.com>
2020-04-02 05:43:25 +0300

    ssssssssss

--- Otus_5/file_system/files/ifile.h
+++ Otus_5/file_system/files/ifile.h
@@ -2,7 +2,7 @@
 #include "error_code/my_error_code.h"
 #include "work_space/canvas.h"
 #include "shape/ishape.h"
-#include <experimental/filesystem> 
+#include <filesystem> 
 //Поддерживаемые файлы
 enum class FileTypes {
     BMP,
@@ -13,21 +13,21 @@ enum class FileTypes {
 //Maybe destroe default constructors?
 struct IFile {
     IFile() : m_Path("") {}
-    IFile(const std::experimental::filesystem::path aPath) : m_Path(aPath) {}
+    IFile(const std::filesystem::path aPath) : m_Path(aPath) {}
 
-    virtual ErrorCode Open(const std::experimental::filesystem::path aPath) = 0;
-    virtual ErrorCode Create(const std::experimental::filesystem::path aPath) = 0;
+    virtual ErrorCode Open(const std::filesystem::path aPath) = 0;
+    virtual ErrorCode Create(const std::filesystem::path aPath) = 0;
     virtual ErrorCode Save() = 0;
     virtual ErrorCode Write() = 0;
     virtual ErrorCode Close() = 0;
     virtual std::unique_ptr<Canvas> GetCanvas() = 0;              // При экспорте файла - файловый мэнеджер получает канвас и набор фигур
     virtual ErrorCode GetShapes(Painter& aPainter) = 0; // При экспорте файла - файловый мэнеджер получает канвас и набор фигур
-    std::experimental::filesystem::path GetFileName() const {
+    std::filesystem::path GetFileName() const {
         return m_Path;
     }
 
     //std::vector<IShape> Transform
     virtual ~IFile() {};
 protected:
-    std::experimental::filesystem::path m_Path;
+    std::filesystem::path m_Path;
 };
