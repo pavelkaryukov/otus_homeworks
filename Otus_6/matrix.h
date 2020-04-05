113c22f072aa30aa8f28e878c0432e3cad744a34
pavelkaryukov <pavelkaryukov@gmail.com>
2020-04-05 22:35:25 +0300

    otus_6. матрица первая проба пера сделан внутренний класс, осталось реализовать обертку

--- Otus_6/matrix.h
+++ Otus_6/matrix.h
@@ -6,26 +6,79 @@
 #include <stdexcept>
 //сделай переопределение оператора = , если значение в нем = default = erase элемент
 
-template<class TValue, TValue DefaultValue = TValue(-1)>
-class TMatrix {
+template<class TValue, TValue _DefaultValue = TValue(-1)>
+class TLine {
+#pragma region Usings
     using index_t = std::uint64_t;
-    TValue m_Value = DefaultValue;
+    using map_t = std::map<index_t, TValue>;
+#pragma endregion
+
+#pragma region Controller   
+    class Controller {
+        map_t*  m_Map   = nullptr;
+        index_t m_Index; 
+        TValue  m_Value;
+    public:
+#pragma region Constructors
+        Controller() = delete;
+        
+        Controller(const index_t aIndex, map_t* aMap) : m_Index(aIndex), m_Map(aMap), m_Value(_DefaultValue){};
+      
+        Controller(const Controller& aController) {
+            auto controller = Controller(aController.m_Index, aController.m_Map);
+            controller.m_Value = aController.m_Value;
+            return controller;
+        }
+#pragma endregion
+
+        Controller& operator=(const TValue aValue) {
+            m_Value = aValue;
+            auto iter = m_Map->find(m_Index);
+            if (aValue == _DefaultValue) {
+                if (iter != m_Map->end())
+                    m_Map->erase(iter);
+                return *this;
+            }
+            (*m_Map)[m_Index] = m_Value;
+            return *this;
+        }
+
+#pragma region CompareOperators
+        bool operator==(const TValue aValue) const {
+            return m_Value == aValue;
+        }
+
+        bool operator==(const Controller aRhs) const {
+            return m_Value == aRhs.m_Value;
+        }
+
+        bool operator!=(const TValue aValue) const {
+            return m_Value != aValue;
+        }
+
+        bool operator!=(const Controller aRhs) const {
+            return m_Value != aRhs.m_Value;
+        }
+
+        Controller operator++(int) {
+            Controller controller = Controller(*this)
+            *this = m_Value + 1;
+            return controller;// сделать конструктор копирования
+        }
+
+        Controller& operator++() {
+            *this = m_Value + 1;
+            return *this;// сделать конструктор копирования
+        }
+    };
+#pragma endregion
+#pragma endregion
+
+    map_t m_MatrixMap;
 public:
-    TMatrix() {}
+    TLine() {}
 
-//     TValue  operator[](index_t aIndex) {
-//         return m_Value;
-//     }
-
-    const TValue&  operator[](index_t) const {
-        std::cout << "ReadOperator\r\n";
-        int stop1 = 0;
-        return m_Value;
-    }
-
-    TValue&  operator[](index_t)  {
-        std::cout << "WriteOperator\r\n";
-        int stop1 = 0;
-        return m_Value;
+    Controller  operator[](const index_t aIndex)  {
+        return Controller(aIndex, &m_MatrixMap);
     }
 };
--- Otus_6/otus_6.cpp
+++ Otus_6/otus_6.cpp
@@ -5,13 +5,22 @@ void TestMatrix() {
     std::string str = "Damka";
     str[0] = 'Z';
     char test = str[0];
-    TMatrix<int, -1> matrix;
+    TLine<int, -1> matrix;
     //auto test = myLine[5];
-    std::cout << matrix[5] << std::endl;
-    int test1 = matrix[5];
-    int test2 = matrix[1];
-    int test3 = matrix[7];
-    matrix[7] = -5;
+   // std::cout << matrix[5u] << std::endl;
+    matrix[5] = 22;
+    matrix[2] = 13;
+    matrix[7] = 2;
+    matrix[5] = -1;
+    matrix[2] = -1;
+    matrix[88] = -1;
+
+    (((matrix[88] = -1) = 2) = -1) = 3;
+
+    auto test1 = matrix[12];
+//     int test2 = matrix[1];
+//     int test3 = matrix[7];
+    //matrix[7] = -5;
     int stop1 = 0;
 }
 
