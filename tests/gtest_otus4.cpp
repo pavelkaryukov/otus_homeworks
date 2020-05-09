59194322d3a36117ef980183a21f3b9f74feefe6
pavelkaryukov <pavelkaryukov@gmail.com>
2020-05-09 14:42:02 +0300

    reveiew

---
+++ tests/gtest_otus4.cpp
@@ -0,0 +1,92 @@
+#include <gtest/gtest.h> // googletest header file
+#include "ip_to_str.h"
+#include <array>
+#include <list>
+#include <vector>
+//-----------------------------------------------------------------------------
+TEST(gtest_print_ip,  PrintTuple)//TODO::Переделать тест
+{
+    std::tuple<int, int, int, int>   tuple1 = { 1, 12, 25, 67 };
+    std::tuple<int, short, std::uint64_t> tuple2 = { 1, 12, 25};
+
+    auto res11 = MyIP::ToStr<int, int, int, int >(tuple1, MyIP::ByteOrder::BigEndian);
+    auto res12 = MyIP::ToStr<int, int, int, int>(tuple1, MyIP::ByteOrder::LittleEndian);
+    auto res21 = MyIP::ToStr<int, short, std::uint64_t >(tuple2);
+    auto res22 = MyIP::ToStr<int, short, std::uint64_t >(tuple2);
+
+    ASSERT_EQ(res11.Code, MyIP::ErrorCode::Success);
+    ASSERT_EQ(res12.Code, MyIP::ErrorCode::Success);
+
+    ASSERT_NE(res21.Code, MyIP::ErrorCode::Success);
+    ASSERT_NE(res22.Code, MyIP::ErrorCode::Success);
+
+    ASSERT_EQ(res11.Result, "1.12.25.67");
+    ASSERT_EQ(res12.Result, "67.25.12.1");
+
+    ASSERT_TRUE(res21.Result.empty());
+    ASSERT_TRUE(res22.Result.empty());
+}
+//-----------------------------------------------------------------------------
+TEST(gtest_print_ip, PrintContainer)
+{
+    std::array <int, 4> array1 = { 1, 12, 25, 67 };
+    std::vector<int>    vect1  = { 1, 12, 25, 67 };
+    std::list  <int>    list1  = { 1, 12, 25, 67 };
+
+    auto resArray11 = MyIP::ToStr<std::array<int, 4>>(array1, MyIP::ByteOrder::BigEndian);
+    auto resArray12 = MyIP::ToStr<std::array<int, 4>>(array1, MyIP::ByteOrder::LittleEndian);
+    auto resVect11  = MyIP::ToStr<std::vector<int>>  (vect1, MyIP::ByteOrder::BigEndian);
+    auto resVect12  = MyIP::ToStr<std::vector<int>>  (vect1, MyIP::ByteOrder::LittleEndian);
+    auto resList11  = MyIP::ToStr<std::list<int>>    (list1, MyIP::ByteOrder::BigEndian);
+    auto resList12  = MyIP::ToStr<std::list<int>>    (list1, MyIP::ByteOrder::LittleEndian);
+
+
+    
+    ASSERT_EQ(resArray11.Code, MyIP::ErrorCode::Success);
+    ASSERT_EQ(resArray12.Code, MyIP::ErrorCode::Success);
+
+    ASSERT_EQ(resVect11 .Code, MyIP::ErrorCode::Success);
+    ASSERT_EQ(resVect12 .Code, MyIP::ErrorCode::Success);
+
+    ASSERT_EQ(resList11 .Code, MyIP::ErrorCode::Success);
+    ASSERT_EQ(resList12 .Code, MyIP::ErrorCode::Success);
+
+    ASSERT_EQ(resArray11.Result, "1.12.25.67");
+    ASSERT_EQ(resArray12.Result, "67.25.12.1");
+
+    ASSERT_EQ(resVect11.Result, "1.12.25.67");
+    ASSERT_EQ(resVect12.Result, "67.25.12.1");
+
+    ASSERT_EQ(resList11.Result, "1.12.25.67");
+    ASSERT_EQ(resList12.Result, "67.25.12.1");
+}
+//-----------------------------------------------------------------------------
+TEST(gtest_print_ip, PrintIntegral)
+{
+    auto test1 = char(-1);
+    auto test2 = short(0);
+    auto test3 = int(2130706433);
+    auto test4 = std::uint64_t(8875824491850138409);
+    
+    auto res1 = MyIP::ToStr<decltype(test1)>(test1);
+    auto res2 = MyIP::ToStr<decltype(test2)>(test2);
+    auto res3 = MyIP::ToStr<decltype(test3)>(test3);
+    auto res4 = MyIP::ToStr<decltype(test4)>(test4);
+
+    ASSERT_EQ(res1.Code, MyIP::ErrorCode::Success);
+    ASSERT_EQ(res2.Code, MyIP::ErrorCode::Success);
+    ASSERT_EQ(res3.Code, MyIP::ErrorCode::Success);
+    ASSERT_EQ(res4.Code, MyIP::ErrorCode::Success);
+    
+    ASSERT_EQ(res1.Result, "255"                        );
+    ASSERT_EQ(res2.Result, "0.0"                        );
+    ASSERT_EQ(res3.Result, "127.0.0.1"                  );
+    ASSERT_EQ(res4.Result, "123.45.67.89.101.112.131.41");
+}
+//-----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
---
+++ tuple_utils.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <tuple>
+///Стянуто https://blog.tartanllama.xyz/exploding-tuples-fold-expressions/
+namespace tuple_utils
+{
+    //-----------------------------------------------------------------------------
+    template <std::size_t... Idx>
+    auto make_index_dispatcher(std::index_sequence<Idx...>)
+    {
+        return [](auto&& f) { (f(std::integral_constant<std::size_t, Idx>{}), ...); };
+    }
+    //-----------------------------------------------------------------------------
+    template <std::size_t N>
+    auto make_index_dispatcher()
+    {
+        return make_index_dispatcher(std::make_index_sequence<N>{});
+    }
+    //-----------------------------------------------------------------------------
+    template <typename Tuple, typename Func>
+    void for_each(const Tuple& t, const Func& f)
+    {
+        constexpr auto n = std::tuple_size<std::decay_t<Tuple>>::value;
+        auto dispatcher = make_index_dispatcher<n>();
+        dispatcher([&f, &t](auto idx) { f(std::get<idx>(t)); });
+    }
+
+    /*template <typename Tuple, typename Func>
+    void for_each(Tuple&& t, Func&& f) {
+        constexpr auto n = std::tuple_size<std::decay_t<Tuple>>::value;
+        auto dispatcher = make_index_dispatcher<n>();
+        dispatcher([&f, &t](auto idx) { f(std::get<idx>(std::forward<Tuple>(t))); });
+    }*/
+} // tuple_utils
