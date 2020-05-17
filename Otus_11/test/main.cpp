6c51d5da6b05390d6931d77b3c14ad942368fe4c
pavelkaryukov <pavelkaryukov@gmail.com>
2020-05-17 22:11:54 +0300

    11 задание с демонстрацие но без тестов, библиотека статичная

--- Otus_11/test/main.cpp
+++ Otus_11/test/main.cpp
@@ -1,12 +1,51 @@
 #include "lib/bulk_async.h" 
+#include <condition_variable>
+#include <mutex>
 
-int main() {
-    auto handl1 = async::connect(3);
+std::condition_variable g_Condition;
+std::mutex g_MutexThread;
+
+void ThreadFunc() {
+    {
+        std::unique_lock<std::mutex> locker(g_MutexThread);
+        g_Condition.wait(locker);
+    }
     std::string testCmd = "cmd1";
+    auto handl1 = async::connect(3);
+    auto handl2 = async::connect(2);
     async::receive(handl1, testCmd.data(), testCmd.size());
     async::receive(handl1, testCmd.data(), testCmd.size());
+
+    async::receive(handl2, testCmd.data(), testCmd.size());
+    async::receive(handl2, testCmd.data(), testCmd.size());
+    async::receive(handl2, testCmd.data(), testCmd.size());
+    async::receive(handl2, testCmd.data(), testCmd.size());
+
     async::receive(handl1, testCmd.data(), testCmd.size());
     async::receive(handl1, testCmd.data(), testCmd.size());
     async::disconnect(handl1);
+
+    async::receive(handl2, testCmd.data(), testCmd.size());
+    async::receive(handl2, testCmd.data(), testCmd.size());
+    async::receive(handl2, testCmd.data(), testCmd.size());
+    async::receive(handl2, testCmd.data(), testCmd.size());
+    async::disconnect(handl2);
+}
+
+int main() {
+    //Создание объектов из разных потоков, должно быть 42 bulk суммарно (42 файла)
+    std::vector<std::thread> threads;
+    for (int i = 0; i <= 6; ++i) {
+        threads.emplace_back(std::thread{ []() { ThreadFunc(); }});
+    }
+    {
+        std::unique_lock<std::mutex> locker(g_MutexThread);
+        g_Condition.notify_all();
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
     return 0;
 }
