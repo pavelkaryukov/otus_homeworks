539171a670e2f9eeb6f514749f35858803dfa5be
pavelkaryukov <pavelkaryukov@gmail.com>
2020-05-25 02:09:12 +0300

    test/test_join.cpp

---
+++ Otus_13/test/test_join.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h> // googletest header file
+#include "dispatcher/dispatcher.h"
+#include <sstream>
+
+std::stringstream MakeExpected() {
+     const std::vector<std::string> kStrs = {
+         "OK",
+         "OK",
+         "ERROR: Unknown command [INsERT]",
+         "ERROR: [Таблица не существует]",
+         "OK",
+         "OK",
+         "ERROR: [Кортеж с этим id уже существет]",
+         "2,,Puskin",
+         "OK",
+         "",
+         "0,Tolstoy,",
+         "3,,Lermontov",
+         "OK",
+         "",
+         "ERROR: [Таблица не существует]",
+         "OK",
+         "OK"
+     };    
+    std::stringstream ss;
+     for (const auto& str : kStrs ){
+         ss << str << std::endl;
+     }
+    return ss;
+}
+TEST(test_join, test_dispatcher) {
+    auto printMutex = std::make_shared<std::mutex>();
+    std::stringstream ss;
+    DispatherSUBD executor1{ ss, printMutex };
+    
+    executor1.ProcessCommand("INSERT A 0 Tolstoy");
+    executor1.ProcessCommand("INSERT A 2");
+    executor1.ProcessCommand("INsERT A 2");
+    executor1.ProcessCommand("INSERT Z 21");
+    executor1.ProcessCommand("INSERT B 2 Puskin");
+    executor1.ProcessCommand("INSERT B 3 Lermontov");
+    executor1.ProcessCommand("INSERT B 2 Dostaevskii");
+
+    executor1.ProcessCommand("INTERSECTION");
+    executor1.ProcessCommand("SYMMETRIC_DIFFERENCE");
+
+    executor1.ProcessCommand("TRUNCATE Z");
+    executor1.ProcessCommand("TRUNCATE A");
+    executor1.ProcessCommand("TRUNCATE B");
+    executor1.Exit();
+    
+    auto str = ss.str();
+    auto str2 = MakeExpected().str();
+
+    ASSERT_STRCASEEQ(str.c_str(), str2.c_str());
+    int stop1 = 0;
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
