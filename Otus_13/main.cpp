a85f91b5cfbf3a1d299cbf48307e8de5b2212c54
pavelkaryukov <pavelkaryukov@gmail.com>
2020-05-25 02:30:25 +0300

    test trouble

--- Otus_13/main.cpp
+++ Otus_13/main.cpp
@@ -1,6 +1,7 @@
 ﻿//#include "rsubd/rsubd.h"
 #include "rsubd/cmd_performer.h"
 #include "dispatcher/dispatcher.h"
+#include <sstream>
 
 void Test() {
     RSUBD mySubd;
@@ -83,8 +84,70 @@ void Test3() {
     int stop1 = 0;
 }
 
+std::stringstream MakeExpected() {
+    const std::vector<std::string> kStrs = {
+        "OK",
+        "OK",
+        "ERROR: Unknown command [INsERT]",
+        "ERROR: [Таблица не существует]",
+        "OK",
+        "OK",
+        "ERROR: [Кортеж с этим id уже существет]",
+        "2,,Puskin",
+        "OK",
+        "",
+        "0,Tolstoy,",
+        "3,,Lermontov",
+        "OK",
+        "",
+        "ERROR: [Таблица не существует]",
+        "OK",
+        "OK"
+    };
+    std::stringstream ss;
+    for (const auto& str : kStrs) {
+        ss << str << std::endl;
+    }
+    return ss;
+}
+
+void MainTest() {
+    auto printMutex = std::make_shared<std::mutex>();
+    std::stringstream ss;
+    {
+        DispatherSUBD executor1{ ss, printMutex };
+
+        executor1.ProcessCommand("INSERT A 0 Tolstoy");
+        executor1.ProcessCommand("INSERT A 2");
+        executor1.ProcessCommand("INsERT A 2");
+        executor1.ProcessCommand("INSERT Z 21");
+        executor1.ProcessCommand("INSERT B 2 Puskin");
+        executor1.ProcessCommand("INSERT B 3 Lermontov");
+        executor1.ProcessCommand("INSERT B 2 Dostaevskii");
+
+        executor1.ProcessCommand("INTERSECTION");
+        executor1.ProcessCommand("SYMMETRIC_DIFFERENCE");
+
+        executor1.ProcessCommand("TRUNCATE Z");
+        executor1.ProcessCommand("TRUNCATE A");
+        executor1.ProcessCommand("TRUNCATE B");
+        executor1.Exit();
+    }
+
+    auto str = ss.str();
+    auto str2 = MakeExpected().str();
+
+    std::cout << "output" << std::endl;
+    std::cout << str << std::endl;
+    std::cout << "end_output" << std::endl;
+
+    std::cout << "expected" << std::endl;
+    std::cout << str2 << std::endl;
+    std::cout << "end_expected" << std::endl;
+}
+
 int main(int argc, char** argv) {
     std::locale::global(std::locale(""));
-    Test3();
+    MainTest();
     return 0;
 }
