14d669eedb85591fc6f01b50bcbc20f2ec886e6d
pavelkaryukov <pavelkaryukov@gmail.com>
2020-06-02 14:41:04 +0300

    coding troublec + cstddef

--- Otus_14/mapper/hasher/hasher_crc32.h
+++ Otus_14/mapper/hasher/hasher_crc32.h
@@ -7,11 +7,11 @@ class HasherCRC : public IHasher<std::size_t> {
 public:
     std::size_t CalcHash(const void* aPtr, const std::size_t aSize, const std::size_t aPrefixSize) override {
         const std::size_t len = (aPrefixSize != 0) ? std::min(aPrefixSize, aSize) : aSize;
-        m_сrc32.process_bytes(aPtr, len);
-        auto res = m_сrc32.checksum();
-        m_сrc32.reset();
+        m_crc32.process_bytes(aPtr, len);
+        auto res = m_crc32.checksum();
+        m_crc32.reset();
         return res;
     }
 private:
-    boost::crc_32_type  m_сrc32;
+    boost::crc_32_type  m_crc32;
 };
--- Otus_14/mapper/hasher/ihasher.h
+++ Otus_14/mapper/hasher/ihasher.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <cstddef>
 template<class Res>
 class IHasher {
 public:
