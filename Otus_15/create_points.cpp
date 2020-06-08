541d2c9379eea7b62f50e81f547fae6831ce5ade
pavelkaryukov <pavelkaryukov@gmail.com>
2020-06-08 21:26:33 +0300

    create point for kkmeans

---
+++ Otus_15/create_points.cpp
@@ -0,0 +1,21 @@
+﻿#include <boost/format.hpp> 
+#include <dlib/rand.h>
+#include <fstream>
+
+int main() {
+    std::ofstream file("stars.txt");
+    if (!file || !file.is_open()) {
+        std::cout << "cant open file stars.txt" << std::endl;
+        return 1;
+    }
+    auto randomizer = dlib::rand();
+    const double minCoord = -100;
+    const double maxCoord = 100;
+
+    for (int i = 0; i < 200; ++i) {
+        const auto x = randomizer.get_double_in_range(minCoord, maxCoord);
+        const auto y = randomizer.get_double_in_range(minCoord, maxCoord);
+        file << boost::format("%1$.2f;%2$.2f") % x % y << std::endl;
+    }
+    return 0;
+}
