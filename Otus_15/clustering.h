afb0565df14c0805edeecf837a8b737b24e9b60c
pavelkaryukov <pavelkaryukov@gmail.com>
2020-06-09 03:18:34 +0300

    otus_15 без тестов

---
+++ Otus_15/clustering.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <boost/format.hpp>
+#include <dlib/clustering.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+namespace cluster {
+    using coord_t = std::pair<double, double>;
+    using sample_t = dlib::matrix<double, 2, 1>;
+    using kernel_t = dlib::radial_basis_kernel<sample_t>;
+
+    void CalcClusters(std::vector<coord_t>& aCoords, const std::size_t aCentersNumber, std::ostream& aOstream) {
+        if (aCoords.empty() || aCentersNumber == 0)
+            return;
+        dlib::kcentroid<kernel_t > kc(kernel_t(0.1), 0.01, 8);
+        dlib::kkmeans<kernel_t> test(kc);
+
+        std::vector<sample_t> samples;
+        std::vector<sample_t> initial_centers;
+        sample_t m;
+        for (const auto& coord : aCoords) {
+            m(0) = coord.first;
+            m(1) = coord.second;
+            samples.push_back(m);
+        }
+        test.set_number_of_centers(aCentersNumber);
+        dlib::pick_initial_centers(aCentersNumber, initial_centers, samples, test.get_kernel());
+        test.train(samples, initial_centers);
+        for (std::size_t i = 0U; i < aCentersNumber; ++i) {
+            std::cout << boost::format("centroid[%1%]; size[%2%];") % i % test.get_kcentroid(i).dictionary_size() << std::endl;
+        }
+        std::vector<unsigned long> assignments = spectral_cluster(kernel_t(0.1), samples, aCentersNumber);
+        
+        const auto size = std::min(assignments.size(), samples.size());
+        for (auto i = 0U; i < size; ++i) {
+            if (aOstream)
+                aOstream << boost::format("%1%;%2%;%3%") % samples[i](0) % samples[i](1) % assignments[i] << std::endl;
+            else 
+                break;;
+        }
+    }
+}
--- Otus_15/main.cpp
+++ Otus_15/main.cpp
@@ -1,9 +1,10 @@
-﻿#include <fstream>
-#include <iostream>
+﻿#include "clustering.h"
+#include <fstream>
 #include <string>
 #include <boost/format.hpp>
- 
-
+#include <boost/tokenizer.hpp>
+using separator_t = boost::char_separator<char>;
+using tokenizer_t = boost::tokenizer<separator_t>;
 std::size_t GetClusterNum(int argc, char** argv) {
     if (argc < 2) {
         std::cout << "Установлено количество кластеров по умолчанию = 1" << std::endl;
@@ -17,18 +18,36 @@ std::size_t GetClusterNum(int argc, char** argv) {
     return num;
 }
 
+bool FillPair(cluster::coord_t& aCoord, std::vector<std::string>& aStrs) {
+    if (aStrs.empty())
+        return false;
+    if (aStrs.size() < 2)
+        return false;
+    aCoord.first  = std::atof(aStrs[0].c_str());
+    aCoord.second = std::atof(aStrs[1].c_str());
+    return true;
+}
+
 int main(int argc, char** argv) {
+    std::locale::global(std::locale(""));
     const auto clusters = GetClusterNum(argc, argv);
     std::string str;
-    std::ofstream file("test.txt");
-    if (!file || !file.is_open()) {
+
+    cluster::coord_t coord = {0, 0};
+    std::vector<cluster::coord_t> coords;
+    while (std::getline(std::cin, str)) {
+        tokenizer_t tokenizer(str, separator_t(";\r\n"));
+        std::vector<std::string> tokens(tokenizer.begin(), tokenizer.end());
+        if (!FillPair(coord, tokens))
+            continue;
+        coords.push_back(coord);
+    }
+    std::ofstream outfile("kkmeans_ex_result.txt");
+    if (!outfile || outfile.is_open()) {
+        std::cout << "Не удается открыть файл" << std::endl;
         return 1;
     }
-
-    while (std::getline(std::cin, str)){
-        file << str << std::endl;
-    }
-    file << boost::format("Number of clusters = [%1%]") % clusters << std::endl; //TODO::DELETE
-    file << "file end" << std::endl; //TODO::DELETE
+    cluster::CalcClusters(coords, 6/*clusters*/, outfile);
+    //тут процедура кластеризации
     return 0;
 }
